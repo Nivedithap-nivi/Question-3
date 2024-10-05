b4fb6887a5336303bbbea28db21ae1ab7f65738b
Nivedithap-nivi <nivedithapnivi1010@outlook.com>
2024-10-05 12:51:17 +0530

    Add project files.

---
+++ DeliverService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    public class DeliverService
+    {
+        public bool PlaceOrder(Product product)
+        {
+            if (product.Stock>0)
+            {
+                return false;
+            }
+            else
+            {
+                throw new ProductOutOfStockException();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                Product product = new Product();
+                DeliverService deliveryService = new DeliverService();
+
+                Console.WriteLine("Enter the product name");
+                product.Name = Console.ReadLine();
+
+                Console.WriteLine("Enter the number of stocks");
+                product.Stock = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    bool result = deliveryService.PlaceOrder(product);
+                    if (result)
+                    {
+                        Console.WriteLine("Order placed successfully");
+                    }
+                }
+                catch (ProductOutOfStockException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Thank You! Shop Again.");
+                }
+            
+        }
+    }
+}
+    
+
